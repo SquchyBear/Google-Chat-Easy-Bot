74a8310e6ef0ea2e21e1b16b6dc64e295a6bfc64
SquchyBear <161305881+SquchyBear@users.noreply.github.com>
2024-04-18 17:57:02 +0800

    Update webhook.py
    Should be basically finished

--- Webhook Interface/webhook.py
+++ Webhook Interface/webhook.py
@@ -1,10 +1,25 @@
 import json
 import http2lib
+from pprint import pprint
 
 class webhook():
     def __init__(self, url:str):
+        '''
+        The webhook url should be in this form:
+        https://chat.googleapis.com/v1/spaces/~/messages?key=~&token=~
+        replace each ~ with the respective text of the url.
+        '''
         self.url = url
 
-    def send(self, message:str):
-        # Code to send message
-    
+    def send(self, message:str, printResponse:bool=False):
+        app_message = {"text": message}
+        message_headers = {"Content-Type": "application/json; charset=UTF-8"}
+        http_obj = Http()
+        response = http_obj.request(
+            uri=self.url,
+            method="POST",
+            headers=message_headers,
+            body=dumps(app_message),
+        )
+        if printResponse:
+            pprint(response)
